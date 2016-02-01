a67b31eaeab906deb946bbee770dfd981fd068ae
tianyl1984 <tianyl1984@qq.com>
2016-02-01 22:43:02 +0800

    添加获取cpu、gpu的util类

--- src/main/java/com/tianyl/pi/Main.java
+++ src/main/java/com/tianyl/pi/Main.java
@@ -1,13 +1,12 @@
 package com.tianyl.pi;
 
-import com.alibaba.fastjson.JSONObject;
+import com.tianyl.pi.util.PiUtil;
 
 public class Main {
 
 	public static void main(String[] args) {
-		JSONObject json = new JSONObject();
-		json.put("result", "OK");
-		System.out.println(json.getString("result"));
+		System.out.println(PiUtil.getCpuTemp());
+		System.out.println(PiUtil.getGpuTemp());
 	}
 
 }
---
+++ src/main/java/com/tianyl/pi/util/PiUtil.java
@@ -0,0 +1,37 @@
+package com.tianyl.pi.util;
+
+public class PiUtil {
+
+	public static Integer getGpuTemp() {
+		String retStr = exe("/opt/vc/bin/vcgencmd measure_temp");
+		if (StringUtil.isNotBlank(retStr)) {
+			return Double.valueOf(Double.valueOf(retStr.replace("temp=", "").replace("'C", "")) * 1000).intValue();
+		}
+		return null;
+	}
+
+	public static Integer getCpuTemp() {
+		String retStr = exe("cat /sys/class/thermal/thermal_zone0/temp");
+		if (StringUtil.isNotBlank(retStr)) {
+			return Integer.valueOf(retStr);
+		}
+		return null;
+	}
+
+	private static String exe(String cmd) {
+		try {
+			Process ps = Runtime.getRuntime().exec(cmd);
+			int ret = ps.waitFor();
+			if (ret == 0) {
+				byte[] bs = IOUtils.toByteArray(ps.getInputStream());
+				return new String(bs);
+			} else {
+				byte[] bs = IOUtils.toByteArray(ps.getErrorStream());
+				System.out.println("error:" + new String(bs));
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+}
